09cc21d43b2c1c98f2ee84746ba1b5f0f2ea7107
Ilya Bagrov <ilya.b.only@gmail.com>
2023-12-10 22:15:37 +0300

    Trying to use TDD

--- task_manager/statuses/tests.py
+++ task_manager/statuses/tests.py
@@ -1,3 +1,80 @@
-from django.test import TestCase
+from datetime import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+from task_manager.statuses.models import StatusModel
+
+
+class StatusTestCase(TestCase):
+    """Testing CRUD operations with Status model"""
+    fixtures = ['user.json']
+    username = 'oleguser'
+    password = 'oLe9$90v'
+
+    def test_status_create(self):
+        name = 'In progress'
+
+        response = self.client.post(
+            reverse('status_create'),
+            data={'name': name}
+        )
+
+        self.assertRedirects(response, reverse('statuses_list'), 302)
+
+        status = StatusModel.objects.get(name=name)
+
+        self.assertEqual(status.name, name)
+        self.assertIsInstance(status.created_at, datetime)
+
+    def test_status_update(self):
+        name = 'In progress'
+
+        self.client.post(
+            reverse('user_login'),
+            data={'username': self.username,
+                  'password': self.password}
+        )
+
+        self.client.post(
+            reverse('status_create'),
+            data={'name': name}
+        )
+
+        status = StatusModel.objects.get(name=name)
+
+        changed_status = 'Finished'
+        response = self.client.post(
+            reverse('status_update', kwargs={'pk': status.pk}),
+            data={'name': changed_status}
+        )
+
+        self.assertRedirects(response, reverse('statuses_list'), 302)
+
+        status.refresh_from_db()
+        self.assertEqual(status.name, changed_status)
+
+    def test_status_delete(self):
+        name = 'In progress'
+
+        self.client.post(
+            reverse('user_login'),
+            data={'username': self.username,
+                  'password': self.password}
+        )
+
+        self.client.post(
+            reverse('status_create'),
+            data={'name': name}
+        )
+
+        status = StatusModel.objects.get(name=name)
+
+        response = self.client.post(
+            reverse('status_delete', kwargs={'pk': status.pk})
+        )
+
+        self.assertRedirects(response, reverse('statuses_list'), 302)
+
+        deleted_status = StatusModel.objects.filter(id=status.pk)
+        self.assertNotIn(status.pk, deleted_status)
