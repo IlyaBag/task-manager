ef6570cf9d38e53c5f1c699b06f7962d440b778e
Ilya Bagrov <ilya.b.only@gmail.com>
2023-12-14 00:53:02 +0300

    Add CRUD tests for statuses (whithout fixtures)

--- task_manager/statuses/tests.py
+++ task_manager/statuses/tests.py
@@ -8,9 +8,23 @@ from task_manager.statuses.models import StatusModel
 
 class StatusTestCase(TestCase):
     """Testing CRUD operations with Status model"""
-    fixtures = ['user.json']
+    # fixtures = ['user.json']
     username = 'oleguser'
     password = 'oLe9$90v'
+
+    def setUp(self) -> None:
+        self.client.post(
+            reverse('user_create'),
+            data={'username': self.username,
+                  'password1': self.password,
+                  'password2': self.password}
+        )
+
+        self.client.post(
+            reverse('user_login'),
+            data={'username': self.username,
+                  'password': self.password}
+        )
 
     def test_status_create(self):
         name = 'In progress'
@@ -20,7 +34,7 @@ class StatusTestCase(TestCase):
             data={'name': name}
         )
 
-        self.assertRedirects(response, reverse('statuses_list'), 302)
+        self.assertRedirects(response, reverse('statuses'), 302)
 
         status = StatusModel.objects.get(name=name)
 
@@ -31,37 +45,25 @@ class StatusTestCase(TestCase):
         name = 'In progress'
 
         self.client.post(
-            reverse('user_login'),
-            data={'username': self.username,
-                  'password': self.password}
-        )
-
-        self.client.post(
             reverse('status_create'),
             data={'name': name}
         )
 
         status = StatusModel.objects.get(name=name)
 
-        changed_status = 'Finished'
+        changed_name = 'Finished'
         response = self.client.post(
             reverse('status_update', kwargs={'pk': status.pk}),
-            data={'name': changed_status}
+            data={'name': changed_name}
         )
 
-        self.assertRedirects(response, reverse('statuses_list'), 302)
+        self.assertRedirects(response, reverse('statuses'), 302)
 
         status.refresh_from_db()
-        self.assertEqual(status.name, changed_status)
+        self.assertEqual(status.name, changed_name)
 
     def test_status_delete(self):
         name = 'In progress'
-
-        self.client.post(
-            reverse('user_login'),
-            data={'username': self.username,
-                  'password': self.password}
-        )
 
         self.client.post(
             reverse('status_create'),
@@ -74,7 +76,53 @@ class StatusTestCase(TestCase):
             reverse('status_delete', kwargs={'pk': status.pk})
         )
 
-        self.assertRedirects(response, reverse('statuses_list'), 302)
+        self.assertRedirects(response, reverse('statuses'), 302)
 
-        deleted_status = StatusModel.objects.filter(id=status.pk)
-        self.assertNotIn(status.pk, deleted_status)
+        deleted_status_qs = StatusModel.objects.filter(name=name)
+        self.assertTrue(len(deleted_status_qs) == 0)
+
+    def test_status_login_required(self):
+        """
+        Сhecking that only authorized users can perform actions with statuses.
+        """
+        self.client.post(reverse('user_logout'))
+
+        # View status list
+        url = reverse('statuses')
+        response = self.client.get(url)
+        self.assertRedirects(
+            response,
+            f"{reverse('user_login')}?next={url}",
+            status_code=302,
+            msg_prefix='Test for a status list'
+        )
+
+        # Status creating
+        url = reverse('status_create')
+        response = self.client.get(url)
+        self.assertRedirects(
+            response,
+            f"{reverse('user_login')}?next={url}",
+            status_code=302,
+            msg_prefix='Test of a status creating'
+        )
+
+        # Status updating
+        url = reverse('status_update', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertRedirects(
+            response,
+            f"{reverse('user_login')}?next={url}",
+            status_code=302,
+            msg_prefix='Test of a status updating'
+        )
+
+        # Status deleting
+        url = reverse('status_delete', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertRedirects(
+            response,
+            f"{reverse('user_login')}?next={url}",
+            status_code=302,
+            msg_prefix='Test of a status deleting'
+        )
