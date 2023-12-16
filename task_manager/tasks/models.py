f4f7a67a85387bb930913f7a2354ff5287e15368
Ilya Bagrov <ilya.b.only@gmail.com>
2023-12-17 01:01:26 +0300

    Create task model

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+from task_manager.statuses.models import StatusModel
+
+
+class TaskModel(models.Model):
+    name = models.CharField(_('Name'), max_length=150)
+    description = models.TextField(_('Description'), null=True)  # необязательное
+    status = models.ForeignKey(  # обязательное
+        StatusModel,
+        on_delete=models.PROTECT,
+        verbose_name = _('Status')
+    )
+    # labels = models.ManyToManyField(
+    #     "LabelModel"
+    # )
+    executor = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        verbose_name=_('Executor')
+    )
+    author = models.ForeignKey(  # обязательное автоматом
+        User,
+        on_delete=models.PROTECT,
+        related_name='author',
+        verbose_name=_('Author'),
+        auto_created=True
+    )
+    created_at = models.DateTimeField(_('Creation date'), auto_now_add=True)
