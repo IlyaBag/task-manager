d20c5a678bfedcd3f386e42d2d44e048a350f886
Ilya Bagrov <ilya.b.only@gmail.com>
2023-12-30 15:00:45 +0300

    Integrate error tracker Rollbar

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -71,6 +71,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    # Rollbar middleware should be the last one
+    'rollbar.contrib.django.middleware.RollbarNotifierMiddleware'
 ]
 
 ROOT_URLCONF = 'task_manager.urls'
@@ -159,3 +161,12 @@ LOGOUT_REDIRECT_URL = 'index_page'
 # Flash message tags
 
 MESSAGE_TAGS = {messages.ERROR: 'danger'}
+
+# Rollbar: error tracker
+
+ROLLBAR = {
+    'access_token': os.getenv('ROLLBAR_ACCESS_TOKEN'), 
+    'environment': 'development' if DEBUG else 'production',
+    'code_version': '1.0',
+    'root': BASE_DIR,
+}
